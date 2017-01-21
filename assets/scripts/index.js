f1baee26d8dbc203a527968b60d70799aab710ed
itungle <itungle@gmail.com>
2017-01-20 23:18:45 -0800

    added index.js & change layout of page

---
+++ assets/scripts/index.js
@@ -0,0 +1,17 @@
+function simulateTyping(statement, element) {
+
+}
+
+
+
+$('document').ready(function(){
+  var helloWorldMsg = "Hello World!";
+  var imMsg = "I'm Tung Le";
+  var selfStatementMsg = "Problem Solver & Life-long Learner";
+  // 
+  // var welcomeBoxWidth = $(".welcome-msg-box").width();
+  // var socialIconBoxWidth = welcomeBoxWidth * 0.85;
+  // console.log(welcomeBoxWidth);
+  // console.log(socialIconBoxWidth);
+  // $(".social-icons-box").css('width', socialIconBoxWidth);
+});
