1182bfa0d677bd863d82363d5e179fa064def9d0
Tung Le <itungle@gmail.com>
2017-01-23 11:43:21 -0800

    about me layout

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,10 +8,10 @@ $('document').ready(function(){
   var helloWorldMsg = "Hello World!";
   var imMsg = "I'm Tung Le";
   var selfStatementMsg = "Problem Solver & Life-long Learner";
-  // 
-  // var welcomeBoxWidth = $(".welcome-msg-box").width();
-  // var socialIconBoxWidth = welcomeBoxWidth * 0.85;
-  // console.log(welcomeBoxWidth);
-  // console.log(socialIconBoxWidth);
-  // $(".social-icons-box").css('width', socialIconBoxWidth);
+
+  var welcomeBoxWidth = $(".welcome-msg-box").width();
+  var socialIconBoxWidth = welcomeBoxWidth * 0.90;
+  console.log(welcomeBoxWidth);
+  console.log(socialIconBoxWidth);
+  $(".social-icons-box").css('width', socialIconBoxWidth);
 });
